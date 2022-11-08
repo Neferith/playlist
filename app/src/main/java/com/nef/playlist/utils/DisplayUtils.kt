9b37b2c5fd6514435c18ecbefafd09218a0caa54
Neferith <vivien.bernard@gmail.com>
2022-11-08 21:09:21 +0100

    - Fix bug d'affichage

--- app/src/main/java/com/nef/playlist/utils/DisplayUtils.kt
+++ app/src/main/java/com/nef/playlist/utils/DisplayUtils.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import android.widget.Toast
 
 fun displayError(context: Context, message: String?) {
-    if (message.isNullOrEmpty()) {
+    if (!message.isNullOrEmpty()) {
         Toast.makeText(context, message, Toast.LENGTH_LONG).show()
     } else {
         Toast.makeText(context, "Unknown error", Toast.LENGTH_LONG).show()
